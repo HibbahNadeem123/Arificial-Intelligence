886961a6af9c274036034096ed6f3d1356ac26ab
Hibbah Nadeem <hibbah1998@gmail.com>
2019-11-29 23:01:43 +0500

    Lists of data

---
+++ HARNAMATOR.py
@@ -0,0 +1,65 @@
+students=['Shahzar','Sajid','Farwa','Ammar','Ahmed Ammar','Gul',
+          'Anas','Wajeha','Ali Hassan Ghori','Mohsin','Rukhma','Hibbah',
+		  'M Uzair','Haseeb','Taha','M Ali Hassan','Bushra','Bisma',
+		  'Aqsa Usman','Ahsan Mustafa','Hassam','Uzair Mehmood','Rabiya',
+		  'Aqsa Mehreen','Duha','Aimen','Noman','Asad','Hammad','Ahsan Ishtiaq',
+		  'Nameera']
+
+male=['Shahzar','Sajid','Ammar','Ahmed Ammar','Gul',
+     'Anas','Ali Hassan Ghori','Mohsin','M Uzair','Haseeb','Taha','M Ali Hassan',
+	 'Ahsan Mustafa','Hassam','Uzair Mehmood','Noman','Asad','Hammad','Ahsan Ishtiaq',]
+
+lessOrEqualtwenty=['Shahzar','Ahmed Ammar','Gul',
+                 'Anas','Wajeha','Ali Hassan Ghori','Rukhma',
+		         'M Uzair','Haseeb','M Ali Hassan','Bushra','Hassam','Uzair Mehmood',
+		         'Noman','Asad','Ahsan Ishtiaq','Nameera']
+			
+programming=['Shahzar','Sajid','Farwa','Ammar','Ahmed Ammar','Gul',
+          'Anas','Wajeha','Rukhma','Hibbah',
+		  'M Uzair','M Ali Hassan','Bushra','Bisma',
+		  'Hassam','Uzair Mehmood','Rabiya',
+		  'Asad','Ahsan Ishtiaq','Nameera']
+
+coffee=['Shahzar','Farwa','Ammar','Anas','Wajeha','Hibbah',
+		  'M Uzair','Haseeb','M Ali Hassan','Bushra','Bisma',
+		  'Aimen','Asad','Hammad']
+
+tea=['Sajid','Ahmed Ammar','Gul','Anas','Mohsin','Rukhma',
+	 'M Uzair','Taha','Aqsa Usman','Ahsan Mustafa','Hassam',
+	 'Uzair Mehmood','Rabiya','Aqsa Mehreen','Noman','Ahsan Ishtiaq','Nameera']
+
+coloredeyes=['Ammar','Mohsin','Ahsan Mustafa','Aimen']
+
+punjabi=['Wajeha','Mohsin','Rukhma','Aqsa Usman','Aqsa Mehreen']
+pashto=['Haseeb','Aimen']
+sindhi=['Shahzar','Duha']
+turkish=['Shahzar']
+
+continental=['Shahzar','Farwa','Wajeha','Hibbah','Bushra','Bisma','Hassam','Aimen','Nameera']
+asian=['Sajid','Farwa','Ammar','Ahmed Ammar','Gul',
+         'Anas','Wajeha','Ali Hassan Ghori','Mohsin','Rukhma','Hibbah',
+		 'M Uzair','Haseeb','Taha','M Ali Hassan',
+		 'Aqsa Usman','Ahsan Mustafa','Uzair Mehmood','Rabiya',
+		 'Aqsa Mehreen','Duha','Aimen','Noman','Asad','Hammad','Ahsan Ishtiaq']
+
+tom=['Ahmed Ammar','Gul','Anas','Wajeha','Haseeb','M Ali Hassan','Ahsan Ishtiaq']
+jerry=['Ahmed Ammar','Taha','Ahsan Mustafa','Hassam','Duha','Ahsan Ishtiaq']
+popeye=['Noman','Asad','Hammad']
+
+book=['Sajid','Farwa','Anas','Wajeha','Haseeb','Taha','Bisma',
+	 'Uzair Mehmood','Rabiya','Aimen','Hammad','Nameera']
+
+movie=['Shahzar','Ammar','Ahmed Ammar','Gul',
+         'Anas','Wajeha','Mohsin','Rukhma','Hibbah',
+		  'M Uzair','Taha','M Ali Hassan','Bushra',
+		  'Aqsa Usman','Ahsan Mustafa','Hassam',
+		  'Aqsa Mehreen','Duha','Aimen','Noman','Asad','Ahsan Ishtiaq']
+
+#hobbies
+dance=['Ali']
+martialart=['Aqsa Usman']
+games=['Ahmed Ammar','Gul','Anas','Mohsin','Hibbah',
+	   'Taha','Ahsan Mustafa','Hassam','Uzair Mehmood','Rabiya',
+	    'Noman','Ahsan Ishtiaq']
+writing=['Ammar']
+cooking=['Wajeha','Hibbah','Aqsa Mehreen','Duha']
